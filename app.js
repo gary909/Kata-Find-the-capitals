53e040f6026fba5fc8875aacee6df704ab3c4172
gary909 <garywhite61@hotmail.com>
2023-05-11 12:50:53 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var capitals = function (word) {
-	// Write your code here
+    let myArr = [];
+    let myCnt = 0;
+    for(let i = 0; i < word.length; i++){
+        if(word[i].toUpperCase() == word[i]){
+            myArr.push(myCnt);
+            myCnt = myCnt + 1;
+        } else {
+            myCnt = myCnt + 1;
+        }
+    }
+    return myArr;
 };
 
 console.log(capitals('CodEWaRs')); // [0,3,4,6]
